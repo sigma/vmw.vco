72ccdec2d2417f7621197290f6a9ee975458ff74
Yann Hodique <yhodique@vmware.com>
2010-10-25 13:17:31 +0200

    proper README and pypi classification

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 import os
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name = "vmw.vco",
     version = "0.3.1",
@@ -14,9 +17,18 @@ setup(
                         'Twisted',
                         ],
 
-    description="Python bindings for the VMware Orchestrator",
-    author="Yann Hodique",
-    author_email="yhodique@vmware.com",
-    url="http://sigma.github.com/vmw.vco",
+    description = "Python bindings for the VMware Orchestrator",
+    long_description=read('README'),
+    author = "VMware, Inc.",
+    author_email = "yhodique@vmware.com",
+    url = "http://sigma.github.com/vmw.vco",
+    keywords = "vmware bindings",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Twisted",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        ],
 
 )
